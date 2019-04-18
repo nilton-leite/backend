8ef0602997165e17d09a68b54980dd0694af29d2
Nilton <nilton.a.junior@hotmail.com>
2019-04-18 15:56:14 -0300

    Mostrar todos os boxes cadastrados

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -15,6 +15,12 @@ class BoxController {
 
         return res.json(box);
     }
+
+    async showAll(req,res) {
+        const box = await Box.find().sort({createdAt: -1});
+
+        return res.json(box);
+    }
 }
 
 module.exports = new BoxController();
